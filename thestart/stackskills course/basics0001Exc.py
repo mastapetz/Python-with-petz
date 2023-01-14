a04039a388334edf46750aff235e2856aa80b069
Peter Aigner <mastapetz@icloud.com>
2023-01-14 23:46:39 +0100

    stack skills kurs basics

---
+++ thestart/stackskills course/basics0001Exc.py
@@ -0,0 +1,8 @@
+# Exc1
+x = 9
+y = 8
+z = 3
+
+print(z, y , x )
+print('z=', z, "y=",y, "x=",x)
+#right click -> run in interactive window
---
+++ thestart/stackskills course/hoursinweek.py
@@ -0,0 +1,6 @@
+daysInWeek = 7 
+hoursPerDay = 24
+
+hoursInWeek = daysInWeek*hoursPerDay
+
+print(hoursInWeek)
