7398a220f767158a642122d198760c43c7ca7227
Peter Aigner <mastapetz@icloud.com>
2023-02-07 18:25:37 +0100

    datatypes 00x

---
+++ thestart/stackskills course/basicsDatatypes.py
@@ -0,0 +1,23 @@
+# temperatures =[9.1, 8.8, 7.5, 6.6,9.9] 
+# print(len(temperatures)) #shows length of list
+# print(temperatures[-4:])  #shows value from index negative 4 to end
+# print(temperatures[:4]) #shows value from index 0 to 4
+# print(temperatures[1:3]) #shows value from index 1 to 3, not including 3
+
+#strings have index system
+# mystring ='hello'
+# print(temperatures[1:3]) #shows el
+# print(temperatures[1]) #shows 2
+# print(temperatures[0]) #shows h
+
+# myObject = ['hello', 1,2,3]
+# print(myObject[0:3])  #works, shows index 0's string
+# print(myObject[0][3])  #shows l
+# print(myObject[1][1]) #shows 'no value'
+# print(myObject[0][:3]) #shows 'hel'
+
+#accessing dictionaries
+studentGradeswName ={"Mary": 9.1, "Sam": 8.8, "John": 7.5}
+#instead of index with integers, index need names
+
+print(studentGradeswName["Sam"]) #shows value of sam
